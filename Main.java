bf79e414175f96c3bcdeaa28576ec4a4083574d7
e <160359596+Mr-Ethan1@users.noreply.github.com>
2024-02-24 13:32:10 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);   
+        int num = kb.nextInt();
+        if(num%3 == 0){
+            System.out.print("Fizz");
+        }
+        if(num%5 == 0) {
+            System.out.print("Buzz");
+        
+        }
+        
+                                                                                                    
+    }
+
+}
